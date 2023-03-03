7a9aac3cc9c5618c9b6752eb0bc08aedac9c2a62
akim-nas <98978747+akim-nas@users.noreply.github.com>
2023-03-03 15:59:53 -0500

    added css, now adding classlist to task list

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-let addItem = (user_task) => {
-    const p_itm = document.createElement("p");
+const addItem = (user_task) => {
+    const p_itm = document.createElement("button");
     const p_txt = document.createTextNode(user_task);
+    p_itm.classList.add("user-task");
     p_itm.appendChild(p_txt);
     document.getElementById("todoList").appendChild(p_itm);
 };
-let getTask = () => {
+const getTask = () => {
     let user_task = prompt("Input task: ");
-    if (user_task != null) {
+    if (user_task != "") {
         addItem(user_task);
     }
 };
--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
-let addItem = (user_task: string): void => {
-    const p_itm: Element = document.createElement("p"); // create para element for remove
+const addItem = (user_task: string): void => {
+    const p_itm: Element = document.createElement("button"); // create para element for remove
     const p_txt: Node = document.createTextNode(user_task); // add the text to para element
 
+    p_itm.classList.add("user-task");
     p_itm.appendChild(p_txt); // append txt to para
+    
     document.getElementById("todoList").appendChild(p_itm); // add to todoList div
 }
 
-let getTask = (): void => {
+const getTask = (): void => {
     let user_task: string = prompt("Input task: "); // ask user for task (duh)
 
-    if (user_task != null) { // if they don't put anything dont pull null basically
+    if (user_task != "") { // if they don't put anything dont pull null basically
         addItem(user_task);
     }
 }
