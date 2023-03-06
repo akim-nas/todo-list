9d1919561023897eadd43acc838ac42b334c763c
akim-nas <98978747+akim-nas@users.noreply.github.com>
2023-03-05 20:57:26 -0500

    gonna try something tmrw, with OOP (sorry)

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const addItem = (user_task) => {
-    const p_itm = document.createElement("button");
-    const p_txt = document.createTextNode(user_task);
-    p_itm.classList.add("user-task");
-    p_itm.appendChild(p_txt);
-    document.getElementById("todoList").appendChild(p_itm);
-};
-const getTask = () => {
-    let user_task = prompt("Input task: ");
-    if (user_task != "") {
-        addItem(user_task);
+class TaskList {
+    constructor() {
+        this.addItem = (user_task) => {
+            const p_itm = document.createElement("button");
+            const p_txt = document.createTextNode(user_task);
+            p_itm.classList.add("user-task");
+            p_itm.appendChild(p_txt);
+            document.getElementById("todoList").appendChild(p_itm);
+        };
+        this.getTask = () => {
+            let task_list = document.getElementById("taskList");
+            let user_task = prompt("Input task: ");
+            if (user_task !== "") {
+                this.addItem(user_task);
+            }
+        };
+        this.removeTask = () => {
+        };
     }
-};
+}
+let List = new TaskList;
 //# sourceMappingURL=index.js.map
